2067cd5e9cf671146a3ba2872372b24f2d505255
atanasster <atanasster@gmail.com>
2018-02-07 05:01:06 -0500

    heroku

--- src/reducers/api.js
+++ src/reducers/api.js
@@ -1,10 +1,12 @@
 import openSocket from 'socket.io-client';
 
+const apiServer = process.env.NODE_ENV === "production" ? '' : 'http://localhost:8568';
+
 function api(state) {
   if (!state) {
     const socket = openSocket('//localhost:8090');
     return {
-      url: 'http://localhost:8568/api',
+      url: `${apiServer}/api`,
       socket,
     };
   }
