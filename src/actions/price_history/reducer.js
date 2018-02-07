5e4abfa2a2d02a6df8111901585407f5164e5954
atanasster <atanasster@gmail.com>
2018-02-07 03:30:53 -0500

    if existing price, replace

--- src/actions/price_history/reducer.js
+++ src/actions/price_history/reducer.js
@@ -5,13 +5,23 @@ const initialState = [];
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case ActionTypes.GET_PRICE_HISTORY_SUCCESS:
-      return [...state, {
+    case ActionTypes.GET_PRICE_HISTORY_SUCCESS: {
+      const idx = state.findIndex(item => (
+        item.exchange === action.exchange &&
+        item.symbol === action.symbol &&
+      item.toSymbol === action.toSymbol));
+      const arr = [...state];
+      if (idx !== -1) {
+        arr.splice(idx, 1);
+      }
+      arr.push({
         symbol: action.symbol,
         toSymbol: action.toSymbol,
         exchange: action.exchange,
         orderBook: action.data,
-      }];
+      });
+      return arr;
+    }
     case ActionTypes.CLEAR_PRICE_HISTORY:
       return [];
     default:
