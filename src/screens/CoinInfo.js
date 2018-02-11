7d2308386964684346d923a51990b64e9ade73b3
atanasster <atanasster@gmail.com>
2018-02-11 14:35:10 -0500

    price chart on info screen

--- src/screens/CoinInfo.js
+++ src/screens/CoinInfo.js
@@ -5,6 +5,7 @@ import { Box } from 'grommet';
 import { requestAllOrderBooks } from '../actions/order_book/actions';
 import CoinPage from '../components/CoinPage';
 import PriceCard from '../components/PriceCard';
+import PriceChart from '../components/PriceChart';
 import OrderBookCard from '../components/OrderBookCard';
 
 
@@ -13,7 +14,7 @@ class CoinInfo extends Component {
     const { symbol, toSymbol, exchange } = this.props.match.params;
     return (
       <CoinPage symbol={symbol} toSymbol={toSymbol} exchange={exchange}>
-        <Box direction='row' full='horizontal'>
+        <Box direction='row' full='horizontal' border='bottom' pad={{ vertical: 'small' }}>
           <Box basis='1/3'>
             <PriceCard symbol={symbol} toSymbol={toSymbol} exchange={exchange} />
           </Box>
@@ -23,6 +24,16 @@ class CoinInfo extends Component {
           <Box basis='1/3'>
             <OrderBookCard symbol={symbol} toSymbol={toSymbol} exchange={exchange} />
           </Box>
+        </Box>
+        <Box full='horizontal' basis='medium' margin={{ vertical: 'medium' }}>
+          <PriceChart
+            symbol={symbol}
+            toSymbol={toSymbol}
+            exchange={exchange}
+            period='day'
+            points={2000}
+          />
+
         </Box>
       </CoinPage>
     );
