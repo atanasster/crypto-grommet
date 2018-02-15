113f3e6fdf51c73c699c5e7dc2430531294bc5fc
atanasster <atanasster@gmail.com>
2018-02-15 17:31:04 -0500

    comment out socket server connection, not used

--- src/actions/api/api.js
+++ src/actions/api/api.js
@@ -1,5 +1,5 @@
-import openSocket from 'socket.io-client';
+// import openSocket from 'socket.io-client';
 
-const server = process.env.NODE_ENV === 'production' ? '' : '//localhost:8568';
+export const server = process.env.NODE_ENV === 'production' ? '' : '//localhost:8568';
 export const apiServer = `${server}/api`;
-export const socketServer = openSocket(`${server}`);
+// export const socketServer = openSocket(`${server}`);
