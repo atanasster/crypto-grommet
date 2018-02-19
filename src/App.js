870e692501f307e534579b96b96d9bb8c1d54260
atanasster <atanasster@gmail.com>
2018-02-19 13:09:58 -0500

    responsive grommet app class

--- src/App.js
+++ src/App.js
@@ -2,97 +2,48 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { Router } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
-import { Grommet, Box, Responsive } from 'grommet';
+import { Box } from 'grommet';
 import Content from './components/Content';
 import configureStore from './store';
 import requestExchanges from './actions/exchanges/actions';
 import requestCoins from './actions/coins/actions';
 import { updateResponsive } from './actions/nav/actions';
-
+import GrommetResponsive from './GrommetResponsive';
 // eslint-disable-next-line no-underscore-dangle
 const preloadedState = window.__PRELOADED_STATE__;
 const history = createHistory();
 const store = configureStore(preloadedState, history);
 
 
-export default class App extends Component {
-  state = {
-    theme: {
-      global: {
-        colors: {
-          brand: '#336699',
-        },
-      },
-      table: {
-      },
+const theme = {
+  global: {
+    colors: {
+      brand: '#336699',
     },
-  };
+  },
+  table: {},
+};
 
+export default class App extends Component {
   componentDidMount() {
     // global lists that are used throughout
     store.dispatch(requestExchanges);
     store.dispatch(requestCoins);
   }
 
-  onResponsive = (nav) => {
-    const mobile = nav === 'narrow';
+  onResponsive = (mobile) => {
     store.dispatch(updateResponsive(mobile));
-    const baseSpacing = mobile ? 16 : 24;
-    this.setState({
-      theme: {
-        ...this.state.theme,
-        global: {
-          ...this.state.theme.global,
-          edgeSize: {
-            none: '0',
-            xsmall: `${baseSpacing / 4}px`,
-            small: `${baseSpacing / 2}px`,
-            medium: `${baseSpacing}px`,
-            large: `${baseSpacing * 2}px`,
-            xlarge: `${baseSpacing * 4}px`,
-          },
-        },
-        heading: {
-          ...this.state.theme.heading,
-          level: {
-            1: {
-              medium: { size: `${baseSpacing * 2}px`, height: 1.125, maxWidth: `${baseSpacing * 48}px` },
-              small: { size: `${baseSpacing}px`, height: 1.333, maxWidth: `${baseSpacing * 24}px` },
-              large: { size: `${baseSpacing * 1.5}px`, height: 1.125, maxWidth: `${baseSpacing * 96}px` },
-            },
-            2: {
-              medium: { size: `${baseSpacing * 1.5}px`, height: 1.23, maxWidth: `${baseSpacing * 36}px` },
-              small: { size: `${baseSpacing * 0.8}px`, height: 1.333, maxWidth: `${baseSpacing * 18}px` },
-              large: { size: `${baseSpacing * 2}px`, height: 1.125, maxWidth: `${baseSpacing * 48}px` },
-            },
-            3: {
-              medium: { size: `${baseSpacing}px`, height: 1.333, maxWidth: `${baseSpacing * 24}px` },
-              small: { size: `${baseSpacing * 0.8}px`, height: 1.333, maxWidth: `${baseSpacing * 18}px` },
-              large: { size: `${baseSpacing * 1.5}px`, height: 1.23, maxWidth: `${baseSpacing * 36}px` },
-            },
-            4: {
-              medium: { size: `${baseSpacing * 0.8}px`, height: 1.333, maxWidth: `${baseSpacing * 18}px` },
-              small: { size: `${baseSpacing * 0.6}px`, height: 1.375, maxWidth: `${baseSpacing * 16}px` },
-              large: { size: `${baseSpacing}px`, height: 1.333, maxWidth: `${baseSpacing * 24}px` },
-            },
-          },
-        },
-      },
-    });
   };
 
   render() {
-    const { theme } = this.state;
     return (
       <Provider store={store}>
         <Router history={history}>
-          <Grommet theme={theme}>
-            <Responsive onChange={this.onResponsive} >
-              <Box style={{ minHeight: '100vh' }}>
-                <Content />
-              </Box>
-            </Responsive>
-          </Grommet>
+          <GrommetResponsive theme={theme} onResponsive={this.onResponsive}>
+            <Box style={{ minHeight: '100vh' }}>
+              <Content />
+            </Box>
+          </GrommetResponsive>
         </Router>
       </Provider>
     );
---
+++ src/GrommetResponsive.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Grommet, Responsive } from 'grommet';
+
+export default class GrommetResponsive extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { theme: props.theme };
+  }
+
+  makeResponsive(mobile) {
+    const { theme } = this.props;
+    const baseSpacing = mobile ? 16 : 24;
+    this.setState({
+      theme: {
+        ...theme.theme,
+        global: {
+          ...theme.global,
+          edgeSize: {
+            none: '0',
+            xsmall: `${baseSpacing / 4}px`,
+            small: `${baseSpacing / 2}px`,
+            medium: `${baseSpacing}px`,
+            large: `${baseSpacing * 2}px`,
+            xlarge: `${baseSpacing * 4}px`,
+          },
+        },
+        heading: {
+          ...theme.heading,
+          level: {
+            1: {
+              medium: { size: `${baseSpacing * 2}px`, height: 1.125, maxWidth: `${baseSpacing * 48}px` },
+              small: { size: `${baseSpacing}px`, height: 1.333, maxWidth: `${baseSpacing * 24}px` },
+              large: { size: `${baseSpacing * 1.5}px`, height: 1.125, maxWidth: `${baseSpacing * 96}px` },
+            },
+            2: {
+              medium: { size: `${baseSpacing * 1.5}px`, height: 1.23, maxWidth: `${baseSpacing * 36}px` },
+              small: { size: `${baseSpacing * 0.8}px`, height: 1.333, maxWidth: `${baseSpacing * 18}px` },
+              large: { size: `${baseSpacing * 2}px`, height: 1.125, maxWidth: `${baseSpacing * 48}px` },
+            },
+            3: {
+              medium: { size: `${baseSpacing}px`, height: 1.333, maxWidth: `${baseSpacing * 24}px` },
+              small: { size: `${baseSpacing * 0.8}px`, height: 1.333, maxWidth: `${baseSpacing * 18}px` },
+              large: { size: `${baseSpacing * 1.5}px`, height: 1.23, maxWidth: `${baseSpacing * 36}px` },
+            },
+            4: {
+              medium: { size: `${baseSpacing * 0.8}px`, height: 1.333, maxWidth: `${baseSpacing * 18}px` },
+              small: { size: `${baseSpacing * 0.6}px`, height: 1.375, maxWidth: `${baseSpacing * 16}px` },
+              large: { size: `${baseSpacing}px`, height: 1.333, maxWidth: `${baseSpacing * 24}px` },
+            },
+          },
+        },
+      },
+    });
+  }
+
+  onResponsive = (nav) => {
+    const mobile = nav === 'narrow';
+    const { onResponsive } = this.props;
+    this.makeResponsive(mobile);
+    if (onResponsive) {
+      onResponsive(mobile);
+    }
+  };
+
+  render() {
+    const { children } = this.props;
+    const { theme } = this.state;
+    return (
+      <Grommet theme={theme}>
+        <Responsive onChange={this.onResponsive} >
+          {children}
+        </Responsive>
+      </Grommet>
+    );
+  }
+}
+
+GrommetResponsive.defaultProps = {
+  onResponsive: undefined,
+  theme: {},
+};
+
+GrommetResponsive.propTypes = {
+  onResponsive: PropTypes.func,
+  theme: PropTypes.object,
+};
