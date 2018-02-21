a9807dc644b3db527e8a550b4e56f478c8618855
atanasster <atanasster@gmail.com>
2018-02-21 00:04:31 -0500

    login actions and reducers

--- src/actions/session/reducer.js
+++ src/actions/session/reducer.js
@@ -11,10 +11,8 @@ export default (state = initialState, action) => {
     case ActionTypes.REQUEST_LOGIN:
       return { ...state, loading: true };
     case ActionTypes.SUCCESS_LOGIN:
-      console.log(action);
       return { ...state, user: action.payload.user, loading: false, error: undefined };
     case ActionTypes.FAILURE_LOGIN:
-      console.log(action);
       return { ...state, user: null, loading: false, error: action.payload.response.message };
     default:
       return state;
