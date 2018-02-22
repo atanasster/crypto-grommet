d819472760896d3724532ff2c8dedc36042cf8b7
atanasster <atanasster@gmail.com>
2018-02-21 23:16:13 -0500

    popup size

--- server/api/auth.js
+++ server/api/auth.js
@@ -7,7 +7,7 @@ export default (config) => {
   const router = Router();
   router.post('/login', (req, res) => login(req, res, config));
   router.post('/signup', signup);
-  router.get('/facebook/start', passport.authenticate('facebook', { session: false, scope: ['email'], display: 'popup' }));
+  router.get('/facebook/start', passport.authenticate('facebook', { session: false, scope: ['email'] }));
   router.get('/facebook/redirect', passport.authenticate('facebook', { session: false }),
     id => generateToken(id, config));
   return router;
