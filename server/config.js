2fb65c509395f7ecf88c76bc50586b17f93050d6
atanasster <atanasster@gmail.com>
2018-02-21 22:41:09 -0500

    oauth popup windows initial commit

--- server/config.js
+++ server/config.js
@@ -4,8 +4,8 @@ const config = {
   'DATABASE_URL': 'postgres://postgres:postgres@localhost:5432/cryptogrommet',
   'JWT_SECRET_KEY': 'CHANGE THIS',
   'JWT_EXPIRES_IN': '3 day',
-  'FACEBOOK_APP_ID': '1922652287768816',
-  'FACEBOOK_APP_SECRET': '76b6db633abdd70d6a597d5d8c5366c9',
+  'FACEBOOK_APP_ID': 'CHANGE THIS',
+  'FACEBOOK_APP_SECRET': 'CHANGE THIS',
 };
 
 export default config;
